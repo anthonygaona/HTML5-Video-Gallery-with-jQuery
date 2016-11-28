d9088c5f1ba31c18cec6e3294c8cd5d33ac93a6c
Anthony Gaona <gaonaanthony@gmail.com>
2016-11-28 00:11:13 -0800

    added 2 options for different user experience on different mobile devices

--- includes/video_gallery.js
+++ includes/video_gallery.js
@@ -9,6 +9,8 @@ $(function() {
 
     });//Each loop
 
+
+    //HTML5 Player code with Flash fallback
     $(".videoLink").click(function() {
         
         var videoFile = $(this).attr('videofile'),
@@ -19,6 +21,44 @@ $(function() {
         var videoCode = '<video width="'+videoWidth+'" height="'+videoHeight+'" controls autoplay autobuffer><source src="video/'+videoFile+'.ogv" type="video/ogg" /><source src="video/'+videoFile+'.mp4" type="video/mp4" /><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="'+videoWidth+'" height="'+(videoHeight+40)+'" id="lynda_video_player" align="middle"><param name="allowScriptAccess" value="sameDomain"><param name="allowFullScreen" value="true"><param name="movie" value="lynda_video_player.swf?videoFile=video/'+videoFile+'.mp4&amp;skinFile=lynda_video_skin.swf&amp;videoFileWidth='+videoWidth+'&amp;videoFileHeight='+videoHeight+'"><param name="quality" value="high"><param name="wmode" value="transparent"><param name="scale" value="noscale"><param name="salign" value="lt"><embed src="lynda_video_player.swf?videoFile=video/'+videoFile+'.mp4&amp;skinFile=lynda_video_skin.swf&amp;videoFileWidth='+videoWidth+'&amp;videoFileHeight='+videoHeight+'" quality="high" width="'+videoWidth+'" height="'+(videoHeight+40)+'" name="lynda_video_player" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" scale="noscale" salign="lt" wmode="transparent" allowfullscreen="true" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></video>';
 
         $("#videoPlayer").html(videoCode);
-    });
+
+
+
+        // Option 1
+        $.fancybox({
+            'transitionIn' : 'fade',
+            'transitionOut' : 'fade',
+            'overlayColor' : '#000',
+            'overlayOpacity' : '.6',
+            'href' : '#videoPlayer'
+        });
+
+        // Activate video tag for Android
+        var checkBrowser = navigator.userAgent.toLowerCase();
+        var isAndroid = checkBrowser.indexOf('android') > -1;
+        
+        if(isAndroid) {
+            $('#videoPlayer source[type*="video/mp4"]').removeAttr('type');
+            $('#videoPlayer video').attr('onclick','this.play();');
+        }
+
+        // Option 2 - comment out option one and Android code above
+        // Detect for both Android, iPhone and iPod, and redirect to video
+        // var checkBrowser = navigator.userAgent.toLowerCase();
+        // var isAndroid = checkBrowser.indexOf('android') > -1;
+        // var isiPhone = checkBrowser.indexOf('iphone') > -1;
+        // var isiPod = checkBrowser.indexOf('ipod') > -1;
+        
+        // if( isAndroid || isiPhone || isiPod ){
+        //     window.location = 'video/'+videoFile+'.mp4';
+        // }else{
+        //     $.fancybox({
+        //     'transitionIn' : 'fade',
+        //     'transitionOut' : 'fade',
+        //     'href' : '#videoPlayer'
+        //     });
+        // }
+
+    });//Click Event
 
 });//DOM Ready
